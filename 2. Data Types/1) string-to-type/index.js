96330a01bf18cf3596b33b4f87346c8afab3945b
Kamyshev-Vitaliy <vitalik.kamyshev.1997@mail.ru>
2021-05-12 16:20:54 +0300

    string-to-type

--- 2. Data Types/1) string-to-type/index.js
+++ 2. Data Types/1) string-to-type/index.js
@@ -1,5 +1,24 @@
 function stringToType(str) {
-  // Напиши свой код здесь
+    // Напиши свой код здесь
+    if(Number(str)){
+        return Number(str);
+    }
+    switch (str) {
+        case 'null':
+            return null;
+        case 'undefined':
+            return undefined;
+        case 'true':
+            return true;
+        case 'false':
+            return false;
+        case '':
+            return '';
+        case '0':
+            return 0;
+        default:
+            return str;
+    }
 };
 
 window.stringToType = stringToType;
