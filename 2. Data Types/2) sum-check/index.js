35e7795e8632599c5d7ed71ef15bd172338602f2
Kamyshev-Vitaliy <vitalik.kamyshev.1997@mail.ru>
2021-05-12 16:24:18 +0300

    sum-check

--- 2. Data Types/2) sum-check/index.js
+++ 2. Data Types/2) sum-check/index.js
@@ -1,5 +1,9 @@
 function sumCheck(a, b) {
   // Напиши свой код здесь
+    if(typeof a === 'number' && typeof b === 'number'){
+        return a + b;
+    }else
+        throw 'Wrong arguments type!';
 }
 
 window.sumCheck = sumCheck;
