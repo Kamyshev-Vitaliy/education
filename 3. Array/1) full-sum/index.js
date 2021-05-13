9fc08f9d6a45b2dc0cde65817fdc6d04f3c30865
Kamyshev-Vitaliy <vitalik.kamyshev.1997@mail.ru>
2021-05-13 16:19:45 +0300

    full-sum

--- 3. Array/1) full-sum/index.js
+++ 3. Array/1) full-sum/index.js
@@ -1,5 +1,9 @@
 function fullSum(...args) {
-  // Напиши свой код здесь
+    // Напиши свой код здесь
+    return args.reduce(function (acc, curVal) {
+        if (typeof curVal !== 'number') throw new Error('Wrong Argument Type')
+        return acc + curVal;
+    }, 0);
 }
 
 window.fullSum = fullSum;
