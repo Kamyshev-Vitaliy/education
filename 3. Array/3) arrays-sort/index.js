2d5acdeca85f58a6a6051934926b37f44241ef1f
Kamyshev-Vitaliy <vitalik.kamyshev.1997@mail.ru>
2021-05-17 17:22:27 +0300

    arrays-sort

--- 3. Array/3) arrays-sort/index.js
+++ 3. Array/3) arrays-sort/index.js
@@ -1,5 +1,10 @@
 function arraysSort(arr) {
   // Напиши свой код здесь
+    const copyA = [...arr];
+    return copyA.sort(function (a,b){
+       return Math.max(...a) - Math.max(...b);
+    });
+
 };
 
 window.arraysSort = arraysSort;
